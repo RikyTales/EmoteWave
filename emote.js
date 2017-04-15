6011669e864af14d88e84389c15540d3015f3b40
RikyTales <imriki96@gmail.com>
2017-04-16 00:20:59 +0200

    made the emote duration way shorter, might make it more performant

--- emote.js
+++ emote.js
@@ -1,13 +1,14 @@
 function Emote(key) {
 
-    this.startTime = new Date().getTime()
     this.x = Math.floor(Math.random(100) * width-100)
     this.y = Math.floor(Math.random(100) * height-100)
     this.img = loadImage(`https://static-cdn.jtvnw.net/emoticons/v1/${key}/3.0`)
     
-    this.offset = 255
-    this.amplitude = 3
     this.angle = 0
+    this.amplitude = 3
+    this.initialFrameCount = frameCount
+    this.frameCount = this.initialFrameCount
+    this.offset = 255
     this.upwardsSpeed = Math.floor((Math.random() * 4) + 1)
 
     this.render = function() {
@@ -18,11 +19,11 @@ function Emote(key) {
         this.y -= this.upwardsSpeed
         this.angle += 0.07
         this.offset -= 2
-        this.now = new Date().getTime()
+        this.frameCount += 1
     }
     
     this.delete = function() {
-        if (this.now > (this.startTime + 5000)) {
+        if (this.frameCount === this.initialFrameCount + 150) {
             currentEmotes.splice(this, 1)
         }
     }
