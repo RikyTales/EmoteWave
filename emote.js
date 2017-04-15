82c089c40997a69f1cd636978cb3df7b1ce46bea
RikyTales <imriki96@gmail.com>
2017-04-15 14:14:25 +0200

    added random upwards speed

--- emote.js
+++ emote.js
@@ -1,22 +1,26 @@
 function Emote(key) {
 
-    this.offset = 255
     this.startTime = new Date().getTime()
-    this.amplitude = 3
-    this.angle = 0
     this.x = Math.floor(Math.random(56) * width-56)
     this.y = Math.floor(Math.random(56) * height-56)
     this.img = loadImage(`https://static-cdn.jtvnw.net/emoticons/v1/${key}/3.0`)
+    
+    this.offset = 255
+    this.amplitude = 3
+    this.angle = 0
+    this.upwardsSpeed = Math.floor((Math.random() * 4) + 1)
 
     this.render = function() {
         image(this.img, this.x, this.y)
         tint(255, this.offset)
+    
         this.x += this.amplitude*sin(this.angle)
-        this.y -= 2
+        this.y -= this.upwardsSpeed
         this.angle += 0.07
         this.offset -= 2
         this.now = new Date().getTime()
     }
+    
     this.delete = function() {
         if (this.now > (this.startTime + 5000)) {
             currentEmotes.splice(this, 1)
