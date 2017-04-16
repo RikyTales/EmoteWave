044ba6b2964364c59e603885460400762d25793d
RikyTales <imriki96@gmail.com>
2017-04-16 16:21:54 +0200

    minor adjustments

--- emote.js
+++ emote.js
@@ -1,7 +1,7 @@
 function Emote(key) {
 
-    this.x = Math.floor(Math.random(100) * width-100)
-    this.y = Math.floor(Math.random(100) * height-100)
+    this.x = random(100, width-100)
+    this.y = random(100, height-300)
     this.img = loadImage(`https://static-cdn.jtvnw.net/emoticons/v1/${key}/3.0`)
     
     this.angle = 0
@@ -9,7 +9,7 @@ function Emote(key) {
     this.initialFrameCount = frameCount
     this.frameCount = this.initialFrameCount
     this.offset = 255
-    this.upwardsSpeed = Math.floor((Math.random() * 4) + 1)
+    this.upwardsSpeed = random(2, 5)
 
     this.render = function() {
         image(this.img, this.x, this.y)
